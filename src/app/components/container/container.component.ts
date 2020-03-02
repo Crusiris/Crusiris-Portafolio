8732e6bf465b9cfd767904fa09ae9f06b4a6e1bf
crusiris <cruzsirys@gmail.com>
2020-03-01 21:41:52 -0300

    Funcionalidad del menu hamburgues

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -12,14 +12,31 @@ mostrar=true
 
   ngOnInit() {
   }
+
   handleScroll(element){
     let currentElement = document.getElementById(element)
     console.log(currentElement)
     currentElement.scrollIntoView({ behavior: "smooth" })
+
+    this.mostrarmenu()
   }
 
-  mostrarmenu(){
-    console.log('hola mundo')
+  mostrarmenu() {
+    var divBarIcon3 = document.getElementById("nav-icon3");
+    if (!this.mostrar) {
+      divBarIcon3.classList.toggle("open");
+      (document.getElementById(
+        "navbar-responsive"
+      ) as HTMLElement).style.transform = "translate3d(0,0,0)";
+
+      this.mostrar = true;
+    } else {
+      (document.getElementById(
+        "navbar-responsive"
+      ) as HTMLElement).style.transform = "translate3d(-100%,0,0)";
+      this.mostrar = false;
+      divBarIcon3.classList.remove("open");
+    }
   }
   
 }
